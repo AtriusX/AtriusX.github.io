b74ec4059100a34a52f9f753023ec08995a6602c
Atrius <atriusedm@gmail.com>
2020-03-24 02:13:08 -0400

    Add more life to the text, more color to bg

--- scripts/fireflies.js
+++ scripts/fireflies.js
@@ -21,7 +21,7 @@ function setup() {
 }
 var counter = 0;
 function draw(){
-  document.body.style.backgroundColor = `hsl(${276 + Math.sin(counter += 0.01) * 30}, 100%, 10%)`
+  document.body.style.backgroundColor = `hsl(${276 + Math.sin(counter += 0.001) * 70}, 100%, 10%)`
   resizeCanvas(window.innerWidth, window.innerHeight);
   background(30, 0, 50, (Math.random() * 100 > 99) ? 50 : 50);
   noStroke();
