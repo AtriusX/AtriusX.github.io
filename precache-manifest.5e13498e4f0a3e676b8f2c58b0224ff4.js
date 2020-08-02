234b5151178c87ae37444043e11ba94c775686fb
Atrius <atriusedm@gmail.com>
2020-08-02 17:42:08 -0400

    Updates

---
+++ precache-manifest.5e13498e4f0a3e676b8f2c58b0224ff4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a73a7743f629ae1b7c3840f37b12f86a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3eb13e217991e9420fd9",
+    "url": "/static/css/main.70e269d6.chunk.css"
+  },
+  {
+    "revision": "245e462135f22031d589",
+    "url": "/static/js/2.8e385d54.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.8e385d54.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3eb13e217991e9420fd9",
+    "url": "/static/js/main.fbbf4ebe.chunk.js"
+  },
+  {
+    "revision": "593d2f967f8aecbb94a6",
+    "url": "/static/js/runtime-main.b4243d02.js"
+  }
+]);
