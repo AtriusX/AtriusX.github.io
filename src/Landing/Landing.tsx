e99ee35e9897e9f701b1c73e7189934c709ebfa2
Atrius <atriusedm@gmail.com>
2020-08-03 05:14:23 -0400

    Update landing component with project links

--- src/Landing/Landing.tsx
+++ src/Landing/Landing.tsx
@@ -1,12 +1,14 @@
 import React from "react";
 import './Landing.css';
-
+import Project from "../Project/Project";
 export default class Landing extends React.Component {
 
     public render() {
         return <div id={"landing"}>
             <span className={"landing_text"}>Hey! I'm Atrius!</span>
-            <span className={"description"}>This site is pretty empty right now. Check back later!</span>
+            <div className={"description"}>Check out some of the projects I've contributed to below~!</div>
+            <Project url={"https://github.com/atriusx/dungeonkit"} name={"DungeonKit"}/>
+            <Project url={"https://github.com/DV8FromTheWorld/JDA"} name={"JDA"}/>
         </div>
     }
 }
