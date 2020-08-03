dd6b50d6657df8bcc23d13a83b87f1042db4eab7
Atrius <atriusedm@gmail.com>
2020-08-03 14:27:24 -0400

    Update project component to prevent dragging

--- src/Project/Project.tsx
+++ src/Project/Project.tsx
@@ -2,5 +2,13 @@ import React from 'react';
 import './Project.css';
 
 export default function(props: { url: string, name: string }) {
-    return <a className={"link container"} href={props.url}>{props.name}</a>   
+    return <a 
+        className={"link container"} 
+        href={props.url} 
+        onMouseDown={(event) => { 
+            if (event.preventDefault) event.preventDefault(); 
+        }}
+    >
+        {props.name}
+    </a>   
 }
