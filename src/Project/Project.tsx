d10a8c11c3ff4f06c196a446fb05ded133ede99e
Atrius <atriusedm@gmail.com>
2020-08-03 12:49:08 -0400

    Make entire button work

--- src/Project/Project.tsx
+++ src/Project/Project.tsx
@@ -2,7 +2,5 @@ import React from 'react';
 import './Project.css';
 
 export default function(props: { url: string, name: string }) {
-    return <div className={"container"}>
-        <a className={"link"} href={props.url}>{props.name}</a>
-    </div>
+    return <a className={"link container"} href={props.url}>{props.name}</a>   
 }
