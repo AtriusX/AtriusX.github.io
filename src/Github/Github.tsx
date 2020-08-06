dc7202fc6b5729ca88b23b1d01deb9a5e4324623
Atrius <atriusedm@gmail.com>
2020-08-05 23:56:46 -0400

    Change export class to function

--- src/Github/Github.tsx
+++ src/Github/Github.tsx
@@ -1,20 +1,16 @@
 import './Github.css';
 import logo from './github.png';
-
 import React from 'react';
 
-export default class Github extends React.Component {
-
-    public render() {
-        return <a className="github" href={"https://github.com/AtriusX"}>
-            <img 
-                draggable={false}
-                src={logo} 
-                alt={"Github"} 
-                onMouseDown={(event) => { 
-                    if (event.preventDefault) event.preventDefault(); 
-                }} 
-            />
-        </a>
-    }
+export default function() {
+    return <a className="github" href={"https://github.com/AtriusX"}>
+        <img 
+            draggable={false}
+            src={logo} 
+            alt={"Github"} 
+            onMouseDown={(event) => { 
+                if (event.preventDefault) event.preventDefault(); 
+            }} 
+        />
+    </a>
 }
