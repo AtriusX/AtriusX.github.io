056967103083ecbe6506213f603452dd298a4e73
Atrius <atriusedm@gmail.com>
2020-08-06 04:30:45 -0400

    Update about page with filler text

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -1,6 +1,10 @@
 import React from 'react';
+import './About.css';
 
 export default function() {
     return <>
+        <div className={"about-container"}>
+            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Iusto mollitia ullam, aperiam blanditiis non sequi! Nemo commodi optio rem neque enim vero voluptatibus! Consequatur velit deleniti officiis ducimus assumenda placeat! Lorem, ipsum dolor sit amet consectetur adipisicing elit. Aliquam quia, repudiandae nisi corporis enim officia eum eaque quaerat at placeat? Harum eligendi necessitatibus possimus modi non rerum dolorem reprehenderit quis?Lorem ipsum dolor, sit amet consectetur adipisicing elit. Nemo eligendi cumque earum, nobis ratione labore excepturi quam? Enim ab, perspiciatis atque laborum iure similique velit amet, placeat id, quo voluptates! Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quam fugiat quidem maiores quae! Nesciunt mollitia dolor veniam inventore quia ut voluptate ad eligendi, provident repudiandae tenetur est ab asperiores eveniet. Lorem ipsum, dolor sit amet consectetur adipisicing elit. Nihil aperiam quia repellendus asperiores praesentium nisi dolorum blanditiis distinctio. Minima, porro tenetur ullam cum corporis quidem voluptas facilis. Quas, consequatur harum?
+        </div>
     </>
 }
