81ed57e8f3a38e7a92d12681d83b801fd1685e57
Atrius <atriusedm@gmail.com>
2020-08-07 00:59:22 -0400

    Create about page

---
+++ src/pages/About.md
@@ -0,0 +1,3 @@
+# Hello World! 👋🏼
+
+This about me section will be improved later! But I'm glad I got it to work!
--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -1,10 +1,24 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './About.css';
+import Markdown from 'react-markdown';
+// @ts-ignore
+import about from './About.md';
 
-export default function() {
-    return <>
-        <div className={"about-container"}>
-            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Iusto mollitia ullam, aperiam blanditiis non sequi! Nemo commodi optio rem neque enim vero voluptatibus! Consequatur velit deleniti officiis ducimus assumenda placeat! Lorem, ipsum dolor sit amet consectetur adipisicing elit. Aliquam quia, repudiandae nisi corporis enim officia eum eaque quaerat at placeat? Harum eligendi necessitatibus possimus modi non rerum dolorem reprehenderit quis?Lorem ipsum dolor, sit amet consectetur adipisicing elit. Nemo eligendi cumque earum, nobis ratione labore excepturi quam? Enim ab, perspiciatis atque laborum iure similique velit amet, placeat id, quo voluptates! Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quam fugiat quidem maiores quae! Nesciunt mollitia dolor veniam inventore quia ut voluptate ad eligendi, provident repudiandae tenetur est ab asperiores eveniet. Lorem ipsum, dolor sit amet consectetur adipisicing elit. Nihil aperiam quia repellendus asperiores praesentium nisi dolorum blanditiis distinctio. Minima, porro tenetur ullam cum corporis quidem voluptas facilis. Quas, consequatur harum?
-        </div>
-    </>
+export default class About extends Component<any, { about: string }> {
+    constructor(props: any) {
+        super(props);
+        this.state = { about: "" };
+    }
+
+    componentWillMount() {
+        fetch(about).then(r => r.text()).then(t => this.setState({ about: t }));
+    }
+    
+    public render() {
+        return <>
+            <div className={"about-container"}>
+                <Markdown source={this.state.about} />
+            </div>
+        </>
+    }
 }
