c23d8d59cc230b6984082783b18db8861e996ff2
Atrius <atriusedm@gmail.com>
2020-08-07 00:59:31 -0400

    Implement blog page

--- src/pages/Blog.tsx
+++ src/pages/Blog.tsx
@@ -1,6 +1,32 @@
-import React from 'react';
+import React, { Component } from 'react';
+import ReactDOM from 'react-dom';
+import posts from '../posts/posts';
+import Markdown from 'react-markdown';
+import './Blog.css';
 
-export default function() {
-    return <>
-    </>
+export default class Blog extends Component<any, [{ name: string, content: string }]> {
+    
+    componentWillMount() {
+        let entries = Object.entries(posts);
+        let i = entries.length;
+        Promise.all(
+            entries.reverse().map(([title, file]) => fetch(file).then(r => [title, r.text()]))
+        ).then(data => {
+            let p = data.map(([title, content]) =>
+                Promise.resolve(content).then(text =>
+                    <div key={i--} className={"blogpost"}>
+                        <h1>{title}</h1>
+                        <Markdown source={text} />
+                    </div>
+                )
+            )
+            Promise.all(p).then(out => {
+                ReactDOM.render(<>{out}</>, document.getElementById("blog-container"));
+            })
+        });
+    }
+
+    public render() {
+        return <div id="blog-container" />
+    }
 }
---
+++ src/posts/posts.ts
@@ -0,0 +1,11 @@
+// @ts-ignore
+import test from './Test.md';
+// @ts-ignore
+import other from './Other.md';
+
+const posts = {
+    "Test": test,
+    "Other": other
+}
+
+export default posts;
