df369d25baa2352ed052a3cf2f079ec98d5be43d
Atrius <atriusedm@gmail.com>
2020-08-07 19:26:32 -0400

    Introduce blogpost component

---
+++ src/Blogpost/Blogpost.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import Markdown from 'react-markdown';
+import './Blogpost.css';
+
+export default function(props: { id?: number, title: string, text: string }) {
+    let out;
+    if (props.id !== undefined) 
+        out = <Link to={`/blog/${props.id}`} className={"blog-title"}>
+            <h1>[{props.id}] {props.title}</h1>
+        </Link> 
+    else 
+        out = <h1>{props.title}</h1>
+    
+    return <div key={props.id} className={"blogpost"}>
+        {out}<br/>
+        <Markdown source={props.text} />
+    </div>
+}
