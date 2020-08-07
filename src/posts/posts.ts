2d024b0b7a9ef86a07de59b3b343290a11981d27
Atrius <atriusedm@gmail.com>
2020-08-07 04:26:39 -0400

    Add another blogpost

--- src/posts/posts.ts
+++ src/posts/posts.ts
@@ -2,10 +2,13 @@
 import test from './Test.md';
 // @ts-ignore
 import other from './Other.md';
+// @ts-ignore
+import third from './Third.md'
 
 const posts = {
     "Test": test,
-    "Other": other
+    "Other": other,
+    "E": third
 }
 
 export default posts;
