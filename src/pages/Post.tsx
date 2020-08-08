bbc12f0b1d43770fca1e878242aea347cbcd3203
Atrius <atriusedm@gmail.com>
2020-08-08 03:22:07 -0400

    Simplify post page

--- src/pages/Post.tsx
+++ src/pages/Post.tsx
@@ -1,30 +1,21 @@
 import React, { Component } from 'react';
-import { validPostID, getPost } from '../services/BlogService';
+import { getPost } from '../services/BlogService';
 
-export default class Post extends Component<any, { post: JSX.Element | undefined }> {
+export default class Post extends Component<any, { post?: JSX.Element }> {
     state = { post: undefined } // Default state for post component
 
     componentDidMount() {
-        const { match: { params }} = this.props;
-        let id = params.id - 1;
-        // Ignore IDs that aren't valid
-        if (!validPostID(id)) {
-            this.setState({ post: undefined });
-            return;
-        }
-        // Process ID into post
-        getPost(id).then(post => this.setState({ post: post }));
+        const { match: { params } } = this.props;
+        getPost(params.id - 1)
+            .then(post => this.setState({ post: post }));
     }
 
     componentDidUpdate(prevProps: any) {
-        if (this.props.location !== prevProps.location) {
-            this.componentDidMount()
-        }
+        if (this.props.location !== prevProps.location)
+            this.componentDidMount();
     }
 
     public render() {
-        return <>
-            {this.state.post !== undefined ? this.state.post : undefined }
-        </>
+        return <>{this.state.post !== undefined ? this.state.post : undefined}</>
     }
 }
