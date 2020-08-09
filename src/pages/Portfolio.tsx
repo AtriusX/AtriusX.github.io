d8f29bc43233401c3ab5ae9b38e0151ae22286cc
Atrius <atriusedm@gmail.com>
2020-08-09 01:40:35 -0400

    Implement 'under construction' page for portfolio

--- src/pages/Portfolio.tsx
+++ src/pages/Portfolio.tsx
@@ -1,6 +1,10 @@
 import React from 'react';
+import './Portfolio.css';
 
 export default function() {
-    return <>
-    </>
+    return <div className={"portfolio-container"}>
+        <h1>This area is still work in progress!</h1>
+        <br />
+        <h3>Thank you for your interest, please check back later.</h3>
+    </div>
 }
