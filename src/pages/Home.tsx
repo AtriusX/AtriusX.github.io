66176d115af2a3e139b3ad92316ba0a51175ef97
Atrius <atriusedm@gmail.com>
2020-08-13 02:54:26 -0400

    Move landing component into home page

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,7 +1,24 @@
 import React from 'react';
-import Landing from '../Landing/Landing';
+import Project from '../Project/Project';
+import Avatar from '../Avatar/Avatar';
 import './Home.css';
 
 export default function() {
-  return <Landing />
+  return <div id={"landing"}>
+    <span className={"landing_text"}>
+        Hey! I'm Atrius!
+    </span>
+    <Avatar
+        url={"https://avatars0.githubusercontent.com/u/7331441?s=460&u=d4025a5731648accb32ec70057dc1df5e514a588&v=4"}
+    />
+    <p className={"description"}>
+        Check out some of the projects I've contributed to below~!
+    </p>
+    <Project 
+        url={"https://github.com/atriusx/dungeonkit"} name={"DungeonKit"}
+    />
+    <Project 
+        url={"https://github.com/DV8FromTheWorld/JDA"} name={"JDA"}
+    />
+  </div>
 }
